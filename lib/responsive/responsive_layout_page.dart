0230bf83fc92421fe1ad5e38871466d871524c79
25Dec <thiennhannguyenhuynh@gmail.com>
2023-09-28 23:15:17 +0700

    1st commit

---
+++ lib/responsive/responsive_layout_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayoutPage extends StatefulWidget {
+  const ResponsiveLayoutPage({super.key});
+
+  @override
+  State<ResponsiveLayoutPage> createState() => _ResponsiveLayoutPageState();
+}
+
+class _ResponsiveLayoutPageState extends State<ResponsiveLayoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
